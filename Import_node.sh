ba9393992224a9d1b5a44e9dc1e40b24d7bbfc1d
Muhammad Nur <116989757+mr9868@users.noreply.github.com>
2025-01-22 03:09:55 +0700

    Update Import_node.sh

--- Import_node.sh
+++ Import_node.sh
@@ -1,1 +1,20 @@
+listCont=($( docker ps -a | awk '{print $NF}' | grep witnet ));
+totalCont=${#listCont[@};
 
+#install witnet
+function witnetInstall(){
+
+   docker run --name witnet${totalCont}_node --runtime runc \
+   -v /root/.witnet:/witnet \
+   -v /root/.witnet/config/privKey${totalCont}.txt:/witnet/privKey${totalCont}.txt \
+   -p $(( 21337 + ${totalCont} )):$(( 21337 + ${totalCont} ))/tcp \
+   --restart always -h ac7785ebf70a \
+   --expose $(( 11212 + ${totalCont} ))/tcp \
+   --expose $(( 21337 + ${totalCont} ))/tcp \
+   --expose $(( 21338 + ${totalCont} ))/tcp \
+   -l org.opencontainers.image.ref.name='ubuntu' \
+   -l org.opencontainers.image.version='22.04' \
+   -e 'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' \
+   -e 'RUST_BACKTRACE=full' \
+   -d --entrypoint "./runner.sh" witnet/witnet-rust:2.0.0-rc.8 '-c' '/tmp/testnet-1/witnet.toml' 'node' 'server' '--master-key-import' '/witnet/privKey${totalCont}.txt'
+}
