0f3e2616412ac53f47a8d4b12242020a9a8e0a6f
Muhammad Nur <116989757+mr9868@users.noreply.github.com>
2025-01-22 13:35:40 +0700

    Update wit_installer.sh

--- wit_installer.sh
+++ wit_installer.sh
@@ -101,22 +101,23 @@ totalCont=${#listCont[@]};
 
 #install witnet
 function witnetInstall(){
-command sudo docker run --name witnet$(( 1 + totalCont ))_node \
-	--runtime runc -v /home/user/.witnet:/.witnet \
-    -p $(( 21337 + ${totalCont} )):$(( 21337 + ${totalCont} ))/tcp \
-    --restart always -h ac7785ebf70a \
-    --expose $(( 11212 + ${totalCont} ))/tcp \
-    --expose $(( 21337 + ${totalCont} ))/tcp \
-    --expose $(( 21338 + ${totalCont} ))/tcp \
-    -l org.opencontainers.image.ref.name='ubuntu' \
-    -l org.opencontainers.image.version='22.04' \
-    -e 'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' \
-    -e 'RUST_BACKTRACE=full' \
-    -d --entrypoint "./runner.sh" witnet/witnet-rust:2.0.0-rc.9 '-c' '/tmp/testnet-1/witnet.toml' 'node' 'server'
-}
+command sudo docker run --name witnet${totalCont}_node --runtime runc \
+   -v /root/.witnet:/witnet \
+   -p $(( 21337 + ${totalCont} )):$(( 21337 + ${totalCont} ))/tcp \
+   --restart always -h ac7785ebf70a \
+   --expose $(( 11212 + ${totalCont} ))/tcp \
+   --expose $(( 21337 + ${totalCont} ))/tcp \
+   --expose $(( 21338 + ${totalCont} ))/tcp \
+   -l org.opencontainers.image.ref.name='ubuntu' \
+   -l org.opencontainers.image.version='22.04' \
+   -e 'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' \
+   -e 'RUST_BACKTRACE=full' \
+   -d --entrypoint "./runner.sh" witnet/witnet-rust:2.0.0-rc.9 \
+   '-c' '/tmp/testnet-1/witnet.toml' 'node' 'server' 
+   }
 
 witnetInstall
-listCommand="List command :\n 1. witnetd $(( 1+ totalCont )) nodeStats\n2. witnetd $(( 1 + totalCont )) balance\n3. witnetd $(( 1+ totalCont )) reputation" 
+listCommand="You must run 'exec bash'\nList command :\n1. witnetd $(( 1+ totalCont )) nodeStats\n2. witnetd $(( 1 + totalCont )) balance\n3. witnetd $(( 1+ totalCont )) reputation" 
 echo -e "Finished ✓\n${listCommand} ";
 source $userSource;
 
