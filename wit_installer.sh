061c18fb7af95b4aab771b95cd6c3c73832b6bdd
Muhammad Nur <116989757+mr9868@users.noreply.github.com>
2025-01-22 11:08:53 +0700

    Update wit_installer.sh

--- wit_installer.sh
+++ wit_installer.sh
@@ -84,9 +84,7 @@ command sudo docker run -d --name witnet${totalCont}_node \
       -c /tmp/testnet-1/witnet.toml node server
 }
 witnetInstall
-listCommand="List command :\n 1. witnetd ${totalCont} nodeStats\n 
-             2. witnetd ${totalCont} balance\n 
-             3. witnetd ${totalCont} reputation" 
-echo -e "Finished \n${listCommand} ";
+listCommand="List command :\n 1. witnetd ${totalCont} nodeStats\n2. witnetd ${totalCont} balance\n 3. witnetd ${totalCont} reputation" 
+echo -e "Finished ✓\n${listCommand} ";
 
 
